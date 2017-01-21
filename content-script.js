d6ae50fe08faf208908de63ae5b2ee47cb3ede7a
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-01-22 05:30:57 +0800

    Added Webcam HUD

--- content-script.js
+++ content-script.js
@@ -9,6 +9,8 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
     }
 });
 
+// createVideoHUD();
+
 function scrollDown() {
     $("html, body").animate({
         scrollTop: $(document).scrollTop() + window.innerHeight - 100
@@ -19,4 +21,28 @@ function scrollUp() {
     $("html, body").animate({
         scrollTop: $(document).scrollTop() - window.innerHeight + 100
     }, 400);
+}
+
+function createVideoHUD() {
+    var video = document.createElement('video');
+    video.setAttribute('id', 'video');
+    video.setAttribute('autoplay', '');
+    video.setAttribute('width', '300');
+    video.style.position = 'fixed';
+    video.style.top = 0;
+    video.style.right = 0;
+    document.body.appendChild(video);
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({ video: true }, handleVideo, videoError);
+    }
+
+    function handleVideo(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    }
+
+    function videoError(e) {
+        alert("Error with webcam video.");
+    }
 }
